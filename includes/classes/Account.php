11f6d53f1379d9071a44b73ad3469d123e16d1e9
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-03-12 21:30:53 -0500

    validate names, email

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -19,13 +19,27 @@
             }
         }
         private function validateFirstName($fn){
-            
+            if(strlen($fn) > 25 || strlen($fn) < 2){
+                array_push($this->errorArray, "Your first name must be between 2-25 characters.");
+                return;
+            }
         }
         private function validateLastName($ln){
-            
+            if(strlen($ln) > 25 || strlen($ln) < 2){
+                array_push($this->errorArray, "Your last name must be between 2-25 characters.");
+                return;
+            }
         }
         private function validateEmails($em, $em2){
-            
+            if($em != $em2){
+              array_push($this->errorArray, "Your emails do not match.");
+                return;  
+            }
+            // makes sure the email has ".com" part
+            if(!filter_var($em, FILTER_VALIDATE_EMAIL)){
+                array_push($this->errorArray, "Email is invalid.");
+                return;
+            }
         }
         private function validatePasswords($pw, $pw2){
             
