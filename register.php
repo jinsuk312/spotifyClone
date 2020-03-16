b4883dbfc311cdb0fc9abb7e6cfd5476e8f59763
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-03-16 01:49:27 -0500

    error check

--- register.php
+++ register.php
@@ -29,29 +29,37 @@
         <form id="registerForm" action="register.php" method="POST">
         <h2>Create your free account.</h2>
         <p>      
-            <?php echo $account->getError("Your username must be between 5-25 characters"); ?>
+            <?php echo $account->getError("Your username must be between 5-25 characters."); ?>
             <label for="username=">Username</label>  
             <input id="username" name="username" type="text" placeholder="e.g bartSimpson" required>  
         </p>
         <p>    
-            <?php echo $account->getError("Your first name must be between 2-25 characters"); ?>
+            <?php echo $account->getError("Your first name must be between 2-25 characters."); ?>
             <label for="firstName=">First name</label>  
             <input id="firstName" name="firstName" type="text" placeholder="e.g Bart" required>  
         </p>
          <p>      
-            <?php echo $account->getError("Your last name must be between 2-25 characters"); ?>
+            <?php echo $account->getError("Your last name must be between 2-25 characters."); ?>
             <label for="lastName=">Last Name</label>  
             <input id="lastName" name="lastName" type="text" placeholder="e.g Simpson" required>  
         </p>
          <p>      
+            <?php echo $account->getError("Your emails do not match."); ?>
+            <?php echo $account->getError("Email is invalid."); ?>
+
             <label for="email=">Email</label>  
             <input id="email" name="email" type="email" placeholder="e.g bart@gmail" required>  
         </p>
-         <p>      
+         <p>     
+            <?php echo $account->getError("Your passwords do not match."); ?>
+            <?php echo $account->getError("Your password can only contain numbers and letters."); ?>
+            <?php echo $account->getError("Your password must be between 5-30 characters."); ?>
+ 
             <label for="email2=">Confirm Email</label>  
             <input id="email2" name="email2" type="email" placeholder="e.g bart@gmail.com" required>  
         </p>
-         <p>      
+         <p>  
+    
             <label for="password=">Password</label>  
             <input id="password" name="password" placeholder="Your password" type="password" required>  
         </p>
