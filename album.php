25aa60afd92312f60884c6bb8345f9878ef9471c
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-03-27 10:50:02 -0500

    display # of songs in album, song class, track name and artist name

--- album.php
+++ album.php
@@ -18,9 +18,49 @@ $artist = $album->getArtist();
 
 	<div class="rightSection">
 		<h2><?php echo $album->getTitle(); ?></h2>
-		<span>By <?php echo $artist->getName(); ?></span>
+		<p>By <?php echo $artist->getName(); ?></p>
+		<p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+
 	</div>
 
+</div>
+
+
+<div class="tracklistContainer">
+	<ul class="tracklist">
+
+		<?php
+		$songIdArray = $album->getSongIds();
+
+		$i = 1;
+		foreach ($songIdArray as $songId) {
+
+			$albumSong = new Song($con, $songId);
+			$albumArtist = $albumSong->getArtist();
+
+			echo "<li class='tracklistRow'>
+					<div class='trackCount'>
+						<img class='play' src='assets/images/icons/play-white.png'>
+						<span class='trackNumber'>$i</span>
+					</div>
+
+
+					<div class='trackInfo'>
+						<span class='trackName'>" . $albumSong->getTitle() . "</span>
+						<span class='artistName'>" . $albumArtist->getName() . "</span>
+					</div>
+
+
+				</li>";
+
+			$i = $i + 1;
+		}
+
+		?>
+
+
+
+	</ul>
 </div>
 
 
